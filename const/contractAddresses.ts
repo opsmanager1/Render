cb349a86858f9b774631103629c7879cef8a6b55
serhii <sergii.lemeshko5871@gmail.com>
2023-10-30 21:00:16 +0000

    first commit

---
+++ const/contractAddresses.ts
@@ -0,0 +1,15 @@
+/** Replace the values below with the addresses of your smart contracts. */
+
+// 1. Set up the network your smart contracts are deployed to.
+// First, import the chain from the package, then set the NETWORK variable to the chain.
+
+// 2. The address of the marketplace V3 smart contract.
+// Deploy your own: https://thirdweb.com/thirdweb.eth/MarketplaceV3
+export const MARKETPLACE_ADDRESS = "0xB0258f92820b114c15608100263bA34a4a2A81Dd";
+
+// 3. The address of your NFT collection smart contract.
+export const NFT_COLLECTION_ADDRESS = "0x1aa708D2dE3E2E9Ee5e34455D72ECd7bf235Fae8";
+
+// (Optional) Set up the URL of where users can view transactions on
+// For example, below, we use Mumbai.polygonscan to view transactions on the Mumbai testnet.
+export const ETHERSCAN_URL = "https://ra-escan.dymscan.live/";
