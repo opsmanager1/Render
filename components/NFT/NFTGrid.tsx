7e16948d5e6e3c082e1c4c834aed500638a6542f
Serhii <135651866+opsmanager1@users.noreply.github.com>
2023-12-11 15:39:27 +0200

    Update NFTGrid.tsx

--- components/NFT/NFTGrid.tsx
+++ components/NFT/NFTGrid.tsx
@@ -5,6 +5,7 @@ import { NFT_COLLECTION_ADDRESS } from "../../const/contractAddresses";
 import Skeleton from "../Skeleton/Skeleton";
 import NFT from "./NFT";
 import styles from "../../styles/Buy.module.css";
+import backgroundImage from '../public/DYMCARD.jpg';
 
 type Props = {
   isLoading: boolean;
